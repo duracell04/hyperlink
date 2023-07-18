33d908fc92cdb0fb0973f1f2899706641bdd045f
tornike14 <nijara.g05@gmail.com>
2023-07-18 22:35:31 +0400

    edit: roadmap layout

--- src/assets/fake-data/data-roadmap.js
+++ src/assets/fake-data/data-roadmap.js
@@ -1,22 +1,13 @@
 export const dataRoadmap = [
-  {
-    id: 1,
-    time: "Future",
-    text1: "Building a sustainable revenue stream via affiliate programs.",
-    text2: "Using revenues for user base expansion and platform improvement.",
-    text3:
-      "Onboarding web tool companies and developers to use the Hyperlink API and platform.",
-    status: "future",
-  },
-  {
-    id: 2,
-    time: "2022",
-    text1: "Year of concept development and refinement.",
-    text2: "Iterative testing and user feedback integration.",
-    text3:
-      "Technical and industry trends closely followed to optimize the concept.",
-    status: "",
-  },
+  // {
+  //   id: 1,
+  //   time: "Future",
+  //   text1: "Building a sustainable revenue stream via affiliate programs.",
+  //   text2: "Using revenues for user base expansion and platform improvement.",
+  //   text3:
+  //     "Onboarding web tool companies and developers to use the Hyperlink API and platform.",
+  //   status: "future",
+  // },
   {
     id: 3,
     time: "2023",
@@ -31,6 +22,15 @@ export const dataRoadmap = [
     text1: "Idea for Hyperlink.Tools was born.",
     text2: "Conceptualization and planning started.",
     text3: "First presentations of the concept launched in September.",
+    status: "",
+  },
+  {
+    id: 2,
+    time: "2022",
+    text1: "Year of concept development and refinement.",
+    text2: "Iterative testing and user feedback integration.",
+    text3:
+      "Technical and industry trends closely followed to optimize the concept.",
     status: "",
   },
 ];
--- src/pages/Roadmap.jsx
+++ src/pages/Roadmap.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Breadcrumbs from "../components/reusable_components/breadcrumbs";
-import { dataRoadmap, dataRoadmapMobile }  from "../assets/fake-data/data-roadmap";
+import {
+  dataRoadmap,
+  dataRoadmapMobile,
+} from "../assets/fake-data/data-roadmap";
 
 import icon from "../assets/images/common/down.svg";
 
@@ -13,27 +16,48 @@ function Roadmap() {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
+              <div className="roadmap-wrapper-style2 ">
+                <div className="roadmap-box-style future mx-auto">
+                  <div className="content">
+                    <h6 className="date text-center">Future</h6>
+                    <ul>
+                      <li>
+                        Building a sustainable revenue stream via affiliate
+                        programs.
+                      </li>
+                      <li>
+                        Using revenues for user base expansion and platform
+                        improvement.
+                      </li>
+                      <li>
+                        Onboarding web tool companies and developers to use the
+                        Hyperlink API and platform.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
               <div
-                className="d-md-flex d-none roadmap-wrapper-style2"
+                className="d-md-flex d-none roadmap-wrapper-style2 mt-5"
                 data-aos="fade-up"
                 data-aos-duration="1200"
               >
                 <div className="left">
                   {dataRoadmap.slice(0, 2).map((item) => {
                     return (
-                      <div key={item.id} className={`roadmap-box-style ${item.status}`}>
+                      <div
+                        key={item.id}
+                        className={`roadmap-box-style ${item.status}`}
+                      >
                         <div className="icon">
                           <img src={icon} alt="" />
                         </div>
                         <div className="content">
                           <h6 className="date">{item.time}</h6>
                           <ul>
-                            <li>
-                            {item.text1}
-                            </li>
-                            <li>
-                            {item.text2}
-                            </li>
+                            <li>{item.text1}</li>
+                            <li>{item.text2}</li>
                             <li>{item.text3}</li>
                           </ul>
                         </div>
@@ -42,21 +66,20 @@ function Roadmap() {
                   })}
                 </div>
                 <div className="right">
-                {dataRoadmap.slice(2, 4).map((item) => {
+                  {dataRoadmap.slice(2, 3).map((item) => {
                     return (
-                      <div key={item.id} className={`roadmap-box-style ${item.status}`}>
+                      <div
+                        key={item.id}
+                        className={`roadmap-box-style ${item.status}`}
+                      >
                         <div className="icon">
                           <img src={icon} alt="" />
                         </div>
                         <div className="content">
                           <h6 className="date">{item.time}</h6>
                           <ul>
-                            <li>
-                            {item.text1}
-                            </li>
-                            <li>
-                            {item.text2}
-                            </li>
+                            <li>{item.text1}</li>
+                            <li>{item.text2}</li>
                             <li>{item.text3}</li>
                           </ul>
                         </div>
@@ -73,19 +96,18 @@ function Roadmap() {
                 <div className="left">
                   {dataRoadmapMobile.map((item) => {
                     return (
-                      <div key={item.id} className={`roadmap-box-style ${item.status}`}>
+                      <div
+                        key={item.id}
+                        className={`roadmap-box-style ${item.status}`}
+                      >
                         <div className="icon">
                           <img src={icon} alt="" />
                         </div>
                         <div className="content">
                           <h6 className="date">{item.time}</h6>
                           <ul>
-                            <li>
-                            {item.text1}
-                            </li>
-                            <li>
-                            {item.text2}
-                            </li>
+                            <li>{item.text1}</li>
+                            <li>{item.text2}</li>
                             <li>{item.text3}</li>
                           </ul>
                         </div>
